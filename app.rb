f64dc3be8f6bb1e6ce7f1bee3b59d4e9050458d4
Cheryl K <cherylkelson@icloud.com>
2019-02-20 19:16:37 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,17 +1,7 @@
 
 class App < Sinatra::Base
   get '/' do
-    "Hello World"
+    "Hello World
   end
   # Define your GET '/' route below and respond with "Hello, World!"
-end
-
-
-class App < Sinatra::Base
-
-  # Define your GET '/' route below and respond with "Hello, World!"
-  get '/' do
-    "Hello, World!"
-  end
-
 end
